39f90d4fe410583b55f77a4389af9b38352203f2
Stein Olsen <stein.olsen@usit.uio.no>
2016-08-22 16:36:29 +0200

    Initial state for observationControl-reducer

--- src/reducers/observationcontrol/index.js
+++ src/reducers/observationcontrol/index.js
@@ -3,13 +3,66 @@ const LOAD_SUCCESS = 'musit/observationcontrol/LOAD_SUCCESS'
 const LOAD_FAIL = 'musit/observationcontrol/LOAD_FAIL'
 
 export const initialState = {
-  data: {
+  data: [{
+    id: 1,
+    eventType: 'controlEvent',
     doneBy: '',
     doneDate: '',
     registeredBy: '',
     registeredDate: '',
     observations: []
-  }
+  },
+    {
+      id: 2,
+      eventType: 'controlEvent',
+      doneBy: '',
+      doneDate: '',
+      registeredBy: '',
+      registeredDate: '',
+      temperatureOK: false,
+      inertAirOK: true,
+      gasOK: true,
+      lightConditionsOK: false,
+      cleaningOK: false,
+      alchoholOK: false,
+      moldFungusOK: true,
+      relativeHumidityOK: true,
+      pestOK: false,
+      storageUnit: false
+    }, {
+      id: 3,
+      eventType: 'observationEvent',
+      doneBy: '',
+      doneDate: '',
+      registeredBy: '',
+      registeredDate: '',
+      observations: []
+    }, {
+      id: 4,
+      eventType: 'controlEvent',
+      doneBy: '',
+      doneDate: '',
+      registeredBy: '',
+      registeredDate: '',
+      temperatureOK: true,
+      inertAirOK: true,
+      gasOK: false,
+      lightConditionsOK: false,
+      cleaningOK: true,
+      alchoholOK: false,
+      moldFungusOK: true,
+      relativeHumidityOK: true,
+      pestOK: true,
+      storageUnit: true
+    }, {
+      id: 5,
+      eventType: 'observationEvent',
+      doneBy: '',
+      doneDate: '',
+      registeredBy: '',
+      registeredDate: '',
+      observations: []
+    }]
 }
 
 const observationControlReducer = (state = initialState, action = {}) => {
