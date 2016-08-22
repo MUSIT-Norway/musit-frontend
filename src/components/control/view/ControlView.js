50264083c5da19dda4df80521ece89226d93fe83
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2016-08-22 20:12:39 +0200

    Added files.

--- src/components/control/view/ControlView.js
+++ src/components/control/view/ControlView.js
@@ -22,7 +22,7 @@ export default class ControlView extends Component {
     ControlMold: 'asterisk',
     ControlPest: 'bug',
     ControlRelativeHumidity: 'tint',
-    ControlTemperature: 'asterisk'
+    ControlTemperature: 'bug'
 
   }
   static typeMap = {
