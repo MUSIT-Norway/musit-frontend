a19b0888c09c8ccf784094d0a0754d9314e3e6f9
Stein Olsen <stein.olsen@usit.uio.no>
2016-08-29 12:19:09 +0200

    Fixed tests for Control and added one simple test for ObservationsControl-reducer.

--- src/reducers/grid/observationcontrol/index.js
+++ src/reducers/grid/observationcontrol/index.js
@@ -5,106 +5,109 @@ const LOAD_ACTOR = 'musit/observationcontrol/LOAD_ACTOR'
 const LOAD_ACTOR_SUCCESS = 'musit/observationcontrol/LOAD_ACTOR_SUCCESS'
 const LOAD_ACTOR_FAILURE = 'musit/observationcontrol/LOAD_ACTOR_FAILURE'
 
-const initialState = {
-  data: [
-    {
-      id: 1,
-      type: 'control',
-      doneDate: '01.01.1983',
-      types: { ControlTemperature: true,
-        ControlHypoxicAir: null,
-        ControlRelativeHumidity: null,
-        ControlCleaning: null,
-        ControlLightingCondition: null,
-        ControlAlcohol: true,
-        ControlGas: null,
-        ControlMold: null,
-        ControlPest: true,
-        envdata: null },
-      doneBy: 'Blablabla...',
-      registeredDate: '01.01.1983',
-      registeredBy: 'Blabla...'
-    },
-    {
-      id: 2,
-      type: 'observation',
-      doneDate: '01.01.1984',
-      types: { ControlTemperature: true,
-        ControlHypoxicAir: null,
-        ControlRelativeHumidity: null,
-        ControlCleaning: null,
-        ControlLightingCondition: true,
-        ControlAlcohol: false,
-        ControlGas: null,
-        ControlMold: null,
-        envdata: null },
-      doneBy: 'Blablabla...',
-      registeredDate: '01.01.1983',
-      registeredBy: 'Blabla...'
-    },
-    {
-      id: 3,
-      type: 'observation',
-      doneDate: '01.01.1984',
-      types: { ControlTemperature: true,
-        ControlHypoxicAir: true,
-        ControlRelativeHumidity: true,
-        ControlCleaning: true,
-        ControlLightingCondition: true,
-        ControlAlcohol: true,
-        ControlGas: true,
-        ControlMold: true,
-        envdata: true },
-      doneBy: 'Blablabla...',
-      registeredDate: '01.01.1983',
-      registeredBy: 'Blabla...'
-    },
-    {
-      id: 4,
-      type: 'control',
-      doneDate: '01.01.1984',
-      types: { ControlTemperature: false,
-        ControlHypoxicAir: false,
-        ControlRelativeHumidity: false,
-        ControlCleaning: false,
-        ControlLightingCondition: false,
-        ControlAlcohol: false,
-        ControlGas: false,
-        ControlMold: false,
-        envdata: false },
-      doneBy: 'Blablabla...',
-      registeredDate: '01.01.1983',
-      registeredBy: 'Blabla...'
-    },
-    {
-      id: 5,
-      type: 'observation',
-      doneDate: '01.01.1984',
-      types: { ControlTemperature: null,
-        ControlHypoxicAir: null,
-        ControlRelativeHumidity: null,
-        ControlCleaning: null,
-        ControlLightingCondition: null,
-        ControlAlcohol: true,
-        ControlGas: null,
-        ControlMold: null,
-        envdata: null },
-      doneBy: 'Blablabla...',
-      registeredDate: '01.01.1983',
-      registeredBy: 'Blabla...'
-    },
-    {
-      id: 6,
-      type: 'control',
-      doneDate: '01.01.1984',
-      types: { ControlTemperature: false
-         },
-      doneBy: 'Blablabla...',
-      registeredDate: '01.01.1983',
-      registeredBy: 'Blabla...'
-    }
-  ]
-}
+const initialState = { data: [] }
+
+//
+// const initialState = {
+//   data: [
+//     {
+//       id: 1,
+//       type: 'control',
+//       doneDate: '01.01.1983',
+//       types: { ControlTemperature: true,
+//         ControlHypoxicAir: null,
+//         ControlRelativeHumidity: null,
+//         ControlCleaning: null,
+//         ControlLightingCondition: null,
+//         ControlAlcohol: true,
+//         ControlGas: null,
+//         ControlMold: null,
+//         ControlPest: true,
+//         envdata: null },
+//       doneBy: 'Blablabla...',
+//       registeredDate: '01.01.1983',
+//       registeredBy: 'Blabla...'
+//     },
+//     {
+//       id: 2,
+//       type: 'observation',
+//       doneDate: '01.01.1984',
+//       types: { ControlTemperature: true,
+//         ControlHypoxicAir: null,
+//         ControlRelativeHumidity: null,
+//         ControlCleaning: null,
+//         ControlLightingCondition: true,
+//         ControlAlcohol: false,
+//         ControlGas: null,
+//         ControlMold: null,
+//         envdata: null },
+//       doneBy: 'Blablabla...',
+//       registeredDate: '01.01.1983',
+//       registeredBy: 'Blabla...'
+//     },
+//     {
+//       id: 3,
+//       type: 'observation',
+//       doneDate: '01.01.1984',
+//       types: { ControlTemperature: true,
+//         ControlHypoxicAir: true,
+//         ControlRelativeHumidity: true,
+//         ControlCleaning: true,
+//         ControlLightingCondition: true,
+//         ControlAlcohol: true,
+//         ControlGas: true,
+//         ControlMold: true,
+//         envdata: true },
+//       doneBy: 'Blablabla...',
+//       registeredDate: '01.01.1983',
+//       registeredBy: 'Blabla...'
+//     },
+//     {
+//       id: 4,
+//       type: 'control',
+//       doneDate: '01.01.1984',
+//       types: { ControlTemperature: false,
+//         ControlHypoxicAir: false,
+//         ControlRelativeHumidity: false,
+//         ControlCleaning: false,
+//         ControlLightingCondition: false,
+//         ControlAlcohol: false,
+//         ControlGas: false,
+//         ControlMold: false,
+//         envdata: false },
+//       doneBy: 'Blablabla...',
+//       registeredDate: '01.01.1983',
+//       registeredBy: 'Blabla...'
+//     },
+//     {
+//       id: 5,
+//       type: 'observation',
+//       doneDate: '01.01.1984',
+//       types: { ControlTemperature: null,
+//         ControlHypoxicAir: null,
+//         ControlRelativeHumidity: null,
+//         ControlCleaning: null,
+//         ControlLightingCondition: null,
+//         ControlAlcohol: true,
+//         ControlGas: null,
+//         ControlMold: null,
+//         envdata: null },
+//       doneBy: 'Blablabla...',
+//       registeredDate: '01.01.1983',
+//       registeredBy: 'Blabla...'
+//     },
+//     {
+//       id: 6,
+//       type: 'control',
+//       doneDate: '01.01.1984',
+//       types: { ControlTemperature: false
+//          },
+//       doneBy: 'Blablabla...',
+//       registeredDate: '01.01.1983',
+//       registeredBy: 'Blabla...'
+//     }
+//   ]
+// }
 
 
 const observationControlGridReducer = (state = initialState, action) => {
