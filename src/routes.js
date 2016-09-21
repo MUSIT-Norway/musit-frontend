3bbaaa0bbac41bb29d4ab352f856e0298de2f6af
Stein Olsen <stein.olsen@usit.uio.no>
2016-09-21 12:39:07 +0200

    Routing

--- src/routes.js
+++ src/routes.js
@@ -54,6 +54,7 @@ export default (store) => {
 
   return (
     <Route component={App}>
+      <IndexRedirect to="/" />
       <Route path="/" component={Login} onEnter={redirectIfLoggedIn} />
       <Route path="/picklist" component={PickListView} />
       <Route path="/magasin" component={StorageUnitsTable} />
