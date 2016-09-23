3014e8dfca2f3f29bc13533ff4a33c075f27fb4a
Stein Olsen <stein.olsen@usit.uio.no>
2016-09-23 13:30:36 +0200

    Cleaning branch 384 to new branch

--- src/layout/Breadcrumb.js
+++ src/layout/Breadcrumb.js
@@ -21,6 +21,7 @@ export default class Breadcrumb extends React.Component {
 
   static defaultProps = {
     nodeTypes: [
+      { type: 'Top', iconName: 'home' },
       { type: 'Organisation', iconName: 'folder' },
       { type: 'Building', iconName: 'folder' },
       { type: 'Room', iconName: 'folder' },
@@ -42,17 +43,20 @@ export default class Breadcrumb extends React.Component {
 
     const isLast = (array, index) => (array.length - 1) === index
     const renderCrumb = (nodeArray) => {
-      return nodes.map((node, index) => {
+      return nodeArray.map((node, index) => {
         let fragment = ''
         let iconFragment = ''
         if (node.type && nodeTypes) {
           const currentType = nodeTypes.find((nodeType) => nodeType.type === node.type)
-          if (currentType) {
+          if (currentType.type !== 'Top') {
             iconFragment = (
-              <FontAwesome name={currentType.iconName} style={{ padding: '2px' }} />
+              <FontAwesome name={currentType.iconName} style={{ padding: '1px' }} />
+            )
+          } else {
+             iconFragment = (
+              <FontAwesome name={currentType.iconName} style={{ 'font-size': 'x-large', padding: '1px' }} />
             )
           }
-        }
 
         if (passive) {
           fragment = (
@@ -86,7 +90,8 @@ export default class Breadcrumb extends React.Component {
     }
     return (
       <div>
-        <span className={styles.crumb}>{divider}</span>{renderCrumb(nodes)}
+        <span className={styles.crumb}>{divider}</span>
+          {renderCrumb([{ id: -1, name: '', type: 'Top', url: '/magasin/root' }, ...nodes])}
       </div>
     )
   }
