686eff774ab16480e6de62b806fdeb3a487d8144
Stein Olsen <stein.olsen@usit.uio.no>
2016-09-26 15:27:24 +0200

    Creating connecting logic

---
+++ src/containers/movehistory/index.js
@@ -0,0 +1,20 @@
+import Language from '../../../components/language'
+import { loadMoveHistoryForObject } from '../../reducers/grid/move'
+import { connect } from 'react-redux';
+import MusitModalImpl from '../../components/formfields/musitModal/musitModal'
+
+const mapStateToProps = (state) => ({
+  translate: (key, markdown) => Language.translate(key, markdown),
+  moves: state.movehistory.data || [],
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return ({
+    loadHistory: () => {
+      dispatch(loadMoveHistoryForObject())
+    } })
+}
+
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class MusitModal extends MusitModalImpl {}
