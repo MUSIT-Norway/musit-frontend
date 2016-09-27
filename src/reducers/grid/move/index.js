2546cb0a3ad16264873f2d06c448e5104da41ec1
Stein Olsen <stein.olsen@usit.uio.no>
2016-09-27 09:16:42 +0200

    Mocking data in reducer

--- src/reducers/grid/move/index.js
+++ src/reducers/grid/move/index.js
@@ -2,7 +2,18 @@ const LOAD = 'musit/movehistory/LOAD'
 const LOAD_SUCCESS = 'musit/movehistory/LOAD_SUCCESS'
 const LOAD_FAIL = 'musit/movehistory/LOAD_FAIL'
 
-const initialState = { data: [] }
+const initialState = { data: [
+  { doneBy: 'Kjell Kjellstad',
+    doneDate: '23.12.2001',
+    from: { path: 'NHM/3etg/Rom 3/Skap 4/Skuff 4/Eske 3' },
+    to: { path: 'NHM/3etg/Rom 3/Skap 2/Skuff 1/Eske 8' }
+  },
+  { doneBy: 'Bjørn Bjørnsen',
+    doneDate: '21.12.2005',
+    from: { path: 'NHM/1 etg/Rom 4/Skap 14/Skuff 1/Eske 10' },
+    to: { path: 'NHM/3 etg/Rom 1/Skap 6/Skuff 4/Eske 18' }
+  }
+] }
 
 const moveHistoryReducer = (state = initialState, action) => {
   switch (action.type) {
