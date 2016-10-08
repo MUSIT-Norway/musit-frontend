73c53a10ba5bdbc6ab6ef381ad3f2822cd096097
Stein Olsen <stein.olsen@usit.uio.no>
2016-10-08 11:57:09 +0200

    Fixing something, MusitModal was lost

--- src/components/grid/ObjectGrid.js
+++ src/components/grid/ObjectGrid.js
@@ -54,78 +54,86 @@ export default class ObjectGrid extends Component {
   }
 
   render() {
-    const { id, translate, tableData } = this.props
+    const { id, translate, tableData, MusitModal } = this.props
     return (
-      <FormGroup>
-        <div>
-          <Table responsive hover condensed>
-            <thead>
-              <tr>
-                <th>
-                  {translate('musit.grid.object.museumsNumber')}
-                </th>
-                <th>
-                  {translate('musit.grid.object.uNumber')}
-                </th>
-                <th>
-                  {translate('musit.grid.object.term')}
-                </th>
-                <th />
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {tableData.map((c, i) =>
-                <tr key={i} id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}`} >
-                  <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_museumNumber`}>
-                    <FontAwesome name="rebel" />
-                    {` ${c.identifier.museumNo}`}
-                  </td>
-                  <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_uNumber`}>
-                    {c.identifier.subNo}
-                  </td>
-                  <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_term`}>
-                    {c.displayName}
-                  </td>
-                  <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_moveHistory`}>
-                    <a
-                      href=""
-                      onClick={(e) => {
-                        e.preventDefault()
-                        this.props.showMoveHistory(c.id)
-                      }}
-                    >
-                      <span className="icon icon-musitmovehistoryicon" />
-                    </a>
-                  </td>
-                  <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_truck`}>
-                    <a
-                      href=""
-                      onClick={(e) => {
-                        e.preventDefault()
-                        this.showModal(c.id, c.displayName)
-                      }}
-                    >
-                      <FontAwesome name="truck" />
-                    </a>
-                  </td>
-                  <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_shoppingCart`}>
-                    <a
-                      href=""
-                      onClick={(e) => {
-                        e.preventDefault()
-                        this.props.onAction('pickObject', c)
-                      }}
-                    >
-                      <FontAwesome name="shopping-cart" />
-                    </a>
-                  </td>
+      <div>
+        <MusitModal
+          show={this.state.showModal}
+          onHide={this.hideModal}
+          onMove={this.moveModal}
+          headerText={this.props.translate('musit.moveModal.moveObjects')}
+        />
+        <FormGroup>
+          <div>
+            <Table responsive hover condensed>
+              <thead>
+                <tr>
+                  <th>
+                    {translate('musit.grid.object.museumsNumber')}
+                  </th>
+                  <th>
+                    {translate('musit.grid.object.uNumber')}
+                  </th>
+                  <th>
+                    {translate('musit.grid.object.term')}
+                  </th>
+                  <th />
+                  <th />
                 </tr>
-              )}
-            </tbody>
-          </Table>
-        </div>
-      </FormGroup>
+              </thead>
+              <tbody>
+                {tableData.map((c, i) =>
+                  <tr key={i} id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}`} >
+                    <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_museumNumber`}>
+                      <FontAwesome name="rebel" />
+                      {` ${c.identifier.museumNo}`}
+                    </td>
+                    <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_uNumber`}>
+                      {c.identifier.subNo}
+                    </td>
+                    <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_term`}>
+                      {c.displayName}
+                    </td>
+                    <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_moveHistory`}>
+                      <a
+                        href=""
+                        onClick={(e) => {
+                          e.preventDefault()
+                          this.props.showMoveHistory(c.id)
+                        }}
+                      >
+                        <span className="icon icon-musitmovehistoryicon" />
+                      </a>
+                    </td>
+                    <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_truck`}>
+                      <a
+                        href=""
+                        onClick={(e) => {
+                          e.preventDefault()
+                          this.showModal(c.id, c.displayName)
+                        }}
+                      >
+                        <FontAwesome name="truck" />
+                      </a>
+                    </td>
+                    <td id={`${id}_${c.identifier.museumNo}_${c.identifier.subNo}_shoppingCart`}>
+                      <a
+                        href=""
+                        onClick={(e) => {
+                          e.preventDefault()
+                          this.props.onAction('pickObject', c)
+                        }}
+                      >
+                        <FontAwesome name="shopping-cart" />
+                      </a>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </Table>
+          </div>
+        </FormGroup>
+      </div>
     )
   }
 }
