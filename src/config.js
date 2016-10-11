be006ee1f236e9f15bae66e8b37dd8168997eeb8
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-10-11 14:32:02 +0200

    Always fake feide

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   isDev: process.env.NODE_ENV === 'development',
-  isFake: process.env.FAKE_FEIDE || false,
+  isFake: process.env.FAKE_FEIDE || true,
   useDevTools: process.env.DEV_TOOLS || false
 };
