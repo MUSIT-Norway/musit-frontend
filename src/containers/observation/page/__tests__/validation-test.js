4979d5aa85f1ef2e5f7892690144bcbfe7d80eec
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-10-17 20:54:07 +0200

    MUSARK-502 Adding more tests

--- src/containers/observation/page/__tests__/validation-test.js
+++ src/containers/observation/page/__tests__/validation-test.js
@@ -1,11 +1,39 @@
 import { validateFromTo } from '../validation'
-import { keys } from 'lodash'
 
-describe('Validation util', () => {
-  it('validateFromTo should work with decimal numbers', () => {
+describe('Validation.validateFromTo', () => {
+  it('should verify that toValue must be greater than fromValue', () => {
     const formProps = {
       fromValue: '42,65',
       toValue: '42,40'
+    }
+    const errors = validateFromTo(formProps, 0, 'type')
+    expect(errors).toMatchSnapshot()
+  })
+
+  it('should not accept fromValue or toValue with dots as comma', () => {
+    const formProps = {
+      fromValue: '44.65',
+      toValue: '48.40'
+    }
+    const errors = validateFromTo(formProps, 0, 'type')
+    expect(errors).toMatchSnapshot()
+  })
+
+  it('should validate that commentsValue cannot be longer than 250 chars', () => {
+    const formProps = {
+      fromValue: '44,65',
+      toValue: '48,40',
+      commentValue: 'x'.repeat(251)
+    }
+    const errors = validateFromTo(formProps, 0, 'type')
+    expect(errors).toMatchSnapshot()
+  })
+
+  it('should validate correct values', () => {
+    const formProps = {
+      fromValue: '44,65',
+      toValue: '48,40',
+      commentValue: 'x'.repeat(250)
     }
     const errors = validateFromTo(formProps, 0, 'type')
     expect(errors).toMatchSnapshot()
