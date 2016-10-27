db5a6a6c5905424cd4a5ce716ce704319799f77c
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2016-10-27 10:44:53 +0200

    Musark 506 (#145)
    * Added test-case for for Auth reducer.
    Adding and fixing Grid>move test-cases
    
    * Added test-cases for observation control reducer.
    
    * Added testcases for the follwing reducer(contine working).
    1) auth
    2) control
    3) grid
    a) move
    d) observationcontrol
    4) move
    5) observation
    6) picklist
    7) reports
    8) storageobject/grid
    9) storageunit/grid
    10) storageunit/mapper
    14) storageunit/panel
    
    * Fixed the test case
    
    * Added the panel test-cases.
    
    * Added test for all the reducers.
    
    * Added the URL logic.

--- src/reducers/storageunit/modal/__tests__/StorageUnitModalReducer-test.js
+++ src/reducers/storageunit/modal/__tests__/StorageUnitModalReducer-test.js
@@ -1,6 +1,13 @@
 import assert from 'assert'
 import deepFreeze from 'deep-freeze'
 
+import * as actions from '../index'
+import reducer from '../index'
+import Config from '../../../../config'
+import request from 'superagent';
+import nocker from 'superagent-nock';
+const nock = nocker(request);
+
 import suReducer, {
   clearRoot
 } from '../index'
@@ -105,5 +112,190 @@ describe('StorageUnitModalReducer', () => {
   it('Clear StorageUnitModalReducer root', () => {
     const state = suReducer(testSubRootState, clearRoot())
     assert(JSON.stringify(state) === JSON.stringify(testSubNoRootState))
+  })
+
+  const loadOneState = {
+    id: 2,
+    name: 'Utviklingsmuseet',
+    isPartOf: 1,
+    path: ',1,2,',
+    pathNames: [
+      {
+        nodeId: 1,
+        name: 'root-node'
+      },
+      {
+        nodeId: 2,
+        name: 'Utviklingsmuseet'
+      }],
+    updatedBy: 123,
+    updatedDate: '2016-01-01T00:00:00+00:00',
+    type: 'Organisation'
+  }
+
+  it('creates LOAD_ROOT_SUCCESS when fetching data has been done', () => {
+    const id = 2
+    const url = `${Config.magasin.urls.storagefacility.baseUrl(1)}/${id}`
+    nock('http://localhost')
+        .get(url)
+        .reply(200, loadOneState)
+    const store = mockStore()
+
+    return store.dispatch(actions.loadRoot(2))
+        .then(() => {
+          expect(store.getActions()).toMatchSnapshot()
+        })
+  })
+
+  it('LOAD_ROOT: no action', () => {
+    expect(
+        reducer(undefined, {})
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_ROOT: initial action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_ROOT
+        })
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_ROOT: success action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_ROOT_SUCCESS,
+          result: {
+            someField: 1
+          }
+        })
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_ROOT: fail action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_ROOT_FAIL,
+          error: Error('LOAD_ROOT: has error.')
+        })
+    ).toMatchSnapshot()
+  })
+
+  const loadSeveralChildState = [
+    {
+      id: 3,
+      name: 'Forskningens hus',
+      isPartOf: 2,
+      path: ',1,2,3,',
+      type: 'Building',
+      updatedBy: 123,
+      updatedDate: '2016-01-01T00:00:00+00:00'
+    }]
+  it('creates LOAD_SEVERAL_SUCCESS when fetching data has been done', () => {
+    const url = `${Config.magasin.urls.storagefacility.baseUrl(1)}/1/children`
+    nock('http://localhost')
+        .get(url)
+        .reply(200, loadSeveralChildState)
+    const store = mockStore()
+
+    return store.dispatch(actions.loadRoot())
+        .then(() => {
+          expect(store.getActions()).toMatchSnapshot()
+        })
+  })
+
+  it('LOAD_SEVERAL: no action', () => {
+    expect(
+        reducer(undefined, undefined)
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_SEVERAL: initial action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_SEVERAL
+        })
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_SEVERAL: success action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_SEVERAL_SUCCESS,
+          result: {
+            someField: 1
+          }
+        })
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_SEVERAL: fail action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_SEVERAL_FAIL,
+          error: Error('LOAD_SEVERAL has error.')
+        })
+    ).toMatchSnapshot()
+  })
+
+  it('creates LOAD_SEVERAL_SUCCESS child when fetching data has been done', () => {
+    const id =1
+    const url = `${Config.magasin.urls.storagefacility.baseUrl(1)}/${id}/children`
+    nock('http://localhost')
+        .get(url)
+        .reply(200, loadSeveralChildState)
+    const store = mockStore()
+
+    return store.dispatch(actions.loadChildren(1))
+        .then(() => {
+          expect(store.getActions()).toMatchSnapshot()
+        })
+  })
+
+  it('LOAD_SEVERAL child: no action', () => {
+    expect(
+        reducer(undefined, undefined)
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_SEVERAL child: initial action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_SEVERAL
+        })
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_SEVERAL child: success action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_SEVERAL_SUCCESS,
+          result: {
+            someField: 1
+          }
+        })
+    ).toMatchSnapshot()
+  })
+
+  it('LOAD_SEVERAL child: fail action', () => {
+    expect(
+        reducer(undefined, {
+          type: actions.LOAD_SEVERAL_FAIL,
+          error: Error('LOAD_SEVERAL child has error.')
+        })
+    ).toMatchSnapshot()
+  })
+
+
+  it('SET_CURRENT action', () => {
+    expect(
+        reducer(loadSeveralChildState, actions.setCurrent(1))
+    ).toMatchSnapshot()
+  })
+
+  it('CLEAR_CURRENT action', () => {
+    expect(
+        reducer(loadSeveralChildState, actions.clearCurrent())
+    ).toMatchSnapshot()
   })
 })
