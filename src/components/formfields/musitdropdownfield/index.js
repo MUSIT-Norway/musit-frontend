a1a9fb0ac802c0850e1d765bab3a8be1883f8a9f
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2016-11-03 13:26:46 +0100

    Added the conditional style to react-select component. Done the user and impact testing

--- src/components/formfields/musitdropdownfield/index.js
+++ src/components/formfields/musitdropdownfield/index.js
@@ -57,10 +57,10 @@ export default class MusitDropDownField extends Component {
 
   render() {
     const lcAddOnPrefix = this.props.addOnPrefix ? <span className="input-group-addon" >{this.props.addOnPrefix}</span> : null;
-
-    const lcPlaceholder = 
+    const lcPlaceholder =
       <Select
         placeholder={this.props.placeHolder}
+        style={this.props.disabled ? { backgroundColor: '#eee' } : undefined }
         disabled={this.props.disabled}
         value={this.props.value}
         options={this.getOptions()}
@@ -68,7 +68,7 @@ export default class MusitDropDownField extends Component {
         data-toggle="tooltip"
         title={this.props.tooltip}
         clearable={false}
-      />;
+      />
 
     const lcHelp = this.props.help ? <span className="input-group-addon" >?</span> : null;
 
