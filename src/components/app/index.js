faac6adf8015979084c08926cec606d089822ffa
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2016-11-07 13:29:17 +0100

    Musark 562 (#173)
    * Added the keys in language file
    Added the text in JQuery dialog
    
    * Implemented review comments.
    
    * Fixed the message.

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -55,8 +55,9 @@ export default class App extends Component {
     };
   }
 
-  showConfirm(title, onYes) {
-    const prompt = `<div>${ I18n.t('musit.texts.showConfirm.message') }</div>`;
+  showConfirm(message, onYes) {
+    const title = I18n.t('musit.texts.deleteNode');
+    const prompt = `<div>${ message }</div>`;
     const $dialog = $(prompt).dialog({
       autoOpen: false,
       modal: true,
@@ -70,7 +71,7 @@ export default class App extends Component {
     $dialog.dialog({
       buttons : [
         {
-          text: I18n.t('musit.texts.showConfirm.confirm'),
+          text: I18n.t('musit.texts.showConfirm.ok'),
           click: function() {
             onYes();
             $(this).dialog("close");
