b06cc584bff6c89893d7907525d2716e8de4773b
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2016-11-07 14:14:42 +0100

    MUSARK-561 Implement user friendly error messages (#175)
    * MUSARK-561 Initial commit
    
    * Translating errors properly
    
    * Fixing linting error
    
    * Updating test snapshot

--- src/errors/handler.js
+++ src/errors/handler.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NotificationSystem from 'react-notification-system';
-import { source, successSource } from './emitter';
+import event$ from './emitter';
 import { I18n } from 'react-i18nify';
 import ReactDOM from 'react-dom';
 import * as logger from 'loglevel';
@@ -14,14 +14,14 @@ const children = (message) =>
     </p>
   </div>;
 
-successSource.subscribe ((s) => {
-  switch(s.type) {
+const handleNotification = (event) => {
+  switch(event.type) {
   case 'deleteSuccess':
     notificationSystem.addNotification({
       level: 'success',
       title: I18n.t('musit.notificationMessages.deleting'),
       position: 'tc',
-      children: children(s.message)
+      children: children(event.message)
     });
     break;
   case 'movedSuccess':
@@ -29,7 +29,7 @@ successSource.subscribe ((s) => {
       level: 'success',
       title: I18n.t('musit.notificationMessages.moving'),
       position: 'tc',
-      children: children(s.message)
+      children: children(event.message)
     });
     break;
   case 'saveSuccess':
@@ -37,25 +37,44 @@ successSource.subscribe ((s) => {
       level: 'success',
       title: I18n.t('musit.notificationMessages.saving'),
       position: 'tc',
-      children: children(s.message)
+      children: children(event.message)
     });
     break;
   default:
     notificationSystem.addNotification({
       level: 'success',
-      children: children(s.message)
+      children: children(event.message)
     });
   }
-});
+};
 
-source.subscribe((e) => {
-  switch(e.type) {
+const getErrorMessage = (status) => {
+  switch(status) {
+  case 404:
+    return I18n.t('musit.errorMainMessages.notFound');
+  case 401:
+    return I18n.t('musit.errorMainMessages.notAuthenticated');
+  case 403:
+    return I18n.t('musit.errorMainMessages.notAllowed');
+  case 400:
+    return I18n.t('musit.errorMainMessages.badRequest');
+  case 500:
+    return I18n.t('musit.errorMainMessages.applicationError');
+  default:
+    return I18n.t('musit.errorMainMessages.errorCode', { status });
+  }
+};
+
+const handleError = (event) => {
+  const error = event.error || event;
+  const type = event.type;
+  switch(type) {
   case 'network':
-    const { response } = e.error;
-    const { req } = response || {};
-    const msg = req ? req.url : e.error.message;
+    const response = error.response;
+    const responseBody = response.body && response.body.message;
+    const msg = responseBody || getErrorMessage(response.status);
     notificationSystem.addNotification({
-      message: msg,
+      autoDismiss: 0,
       level: 'error',
       title: I18n.t('musit.errorMainMessages.networkError'),
       position: 'tc',
@@ -64,42 +83,60 @@ source.subscribe((e) => {
     break;
   case 'dateValidationError':
     notificationSystem.addNotification({
+      autoDismiss: 0,
       level: 'error',
       title: I18n.t('musit.errorMainMessages.applicationError'),
       position: 'tc',
-      children: children(e.message)
+      children: children(error.message)
     });
     break;
   case 'errorOnDelete':
     notificationSystem.addNotification({
+      autoDismiss: 0,
       level: 'error',
       title: I18n.t('musit.errorMainMessages.applicationError'),
       position: 'tc',
-      children: children(e.message)
+      children: children(error.message)
     });
     break;
   case 'errorOnMove':
     notificationSystem.addNotification({
+      autoDismiss: 0,
       level: 'error',
       title: I18n.t('musit.errorMainMessages.applicationError'),
       position: 'tc',
-      children: children(e.message)
+      children: children(error.message)
     });
     break;
   case 'errorOnSave':
     notificationSystem.addNotification({
+      autoDismiss: 0,
       level: 'error',
       title: I18n.t('musit.errorMainMessages.applicationError'),
       position: 'tc',
-      children: children(e.message)
+      children: children(error.message)
     });
     break;
   default:
-    logger.debug(e.error);
+    logger.debug(error);
     notificationSystem.addNotification({
-      message: e.error.message,
+      autoDismiss: 0,
       level: 'error',
-      position: 'tc'
+      title: I18n.t('musit.errorMainMessages.applicationError'),
+      position: 'tc',
+      children: children(error.message)
     });
+  }
+};
+
+event$.subscribe((event) => {
+  switch(event.type) {
+  case 'musitNotification':
+    handleNotification(event.payload);
+    break;
+  case 'musitError':
+    handleError(event.payload);
+    break;
+  default:
   }
 });
