54b0311d03c9f84f3943edfdb34a721018b92333
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2016-11-09 10:34:31 +0100

    MUSARK-336 - Fixing popups for IE and Firefox (#179)

--- src/components/login-button/real.js
+++ src/components/login-button/real.js
@@ -15,32 +15,29 @@ export default class FeideButton extends React.Component {
         bsStyle="default"
         style={{ marginTop: '1em' }}
         height="20"
-      >
-        <span
-          onClick={() => {
-            const url = `/musit?t=${new Date().getTime()}`;
-            const webWindow = window.open(url, '_blank',
-              'menubar=yes,location=no,scrollbars=yes,width=800,height=600,' +
-               'status=no,resizable=yes,top=0,left=0,dependent=yes,alwaysRaised=yes');
-            if (!webWindow || webWindow.closed || typeof webWindow.closed === 'undefined') {
-              /* eslint-disable no-alert */
-              alert('Enable popups for login to work properly');
+        onClick={() => {
+          const url = `/musit?t=${new Date().getTime()}`;
+          const webWindow = window.open(url, '_blank',
+            'menubar=yes,location=no,scrollbars=yes,width=800,height=600,' +
+             'status=no,resizable=yes,top=0,left=0,dependent=yes,alwaysRaised=yes');
+          if (!webWindow || webWindow.closed || typeof webWindow.closed === 'undefined') {
+            /* eslint-disable no-alert */
+            alert('Enable popups for login to work properly');
+          }
+          webWindow.opener = window;
+          webWindow.focus();
+          const checkLoaded = () => {
+            if (webWindow.closed === true) {
+              const user = jwtDecode(localStorage.getItem('jwtToken'));
+              this.props.setUser(user);
+            } else {
+              setTimeout(checkLoaded, 500);
             }
-            webWindow.opener = window;
-            webWindow.focus();
-            const checkLoaded = () => {
-              if (webWindow.closed === true) {
-                const user = jwtDecode(localStorage.getItem('jwtToken'));
-                this.props.setUser(user);
-              } else {
-                setTimeout(checkLoaded, 500);
-              }
-            };
-            checkLoaded();
-          }}
-        >
-          {this.props.children}
-        </span>
+          };
+          checkLoaded();
+        }}
+      >
+        {this.props.children}
       </Button>
     );
   }
