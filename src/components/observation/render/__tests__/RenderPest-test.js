172e8f90809bc259967251fe116ba3baadf60925
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2016-11-09 13:55:16 +0100

    Implemented different Enzyme unit test cases with render and beforeEach.

--- src/components/observation/render/__tests__/RenderPest-test.js
+++ src/components/observation/render/__tests__/RenderPest-test.js
@@ -1,69 +1,88 @@
-import { shallow } from 'enzyme';
-import MusitField from '../../../../components/formfields/musitfield';
-import MusitDropDownField from '../../../../components/formfields/musitdropdownfield';
+import { render } from 'enzyme';
 import React from 'react';
 import { RenderPest } from '../index';
-import ObservationDoubleTextAreaComponent from '../ObservationDoubleTextAreaComponent';
-import { ControlLabel } from 'react-bootstrap';
 
 describe('RenderPest', () => {
-  const observations = [
-    {
-      count: '1',
-      lifeCycle: 'adult'
-    },
-    {
-      count: "20",
-      lifeCycle: "puppe"
-    },
-    {
-      count: "10",
-      lifeCycle: "larva"
-    }
-  ];
+  let wrapper;
+  beforeEach(() => {
+    const observations = [
+      {
+        count: '1',
+        lifeCycle: 'adult'
+      },
+      {
+        count: "20",
+        lifeCycle: "puppe"
+      },
+      {
+        count: "10",
+        lifeCycle: "larva"
+      }
+    ];
 
-  const ValueProps = {
-    observations : observations,
-    identificationValue : "Identification value.",
-    commentValue : "Comment value."
-  };
+    const ValueProps = {
+      observations : observations,
+      identificationValue : "Identification value.",
+      commentValue : "Comment value."
+    };
 
-  const layoutProps = {
-    lifeCycleWidth: 2,
-    countWidth: 2,
-    removeIconWidth: 1,
-    addIconWidth: 1,
-    commentsLeftWidth: 5,
-    commentsRightWidth: 5
-  };
+    const layoutProps = {
+      lifeCycleWidth: 2,
+      countWidth: 2,
+      removeIconWidth: 1,
+      addIconWidth: 1,
+      commentsLeftWidth: 5,
+      commentsRightWidth: 5
+    };
 
-  it('renders pest observation', () => {
-    const wrapper = shallow(
-      <RenderPest
-        index={1}
-        valueProps={ValueProps}
-        disabled={false}
-        layoutProps={layoutProps}
-      />
-    );
-    // console.log(wrapper);
-    // console.log(wrapper.find(ObservationDoubleTextAreaComponent).length);
-    expect(wrapper.find(ObservationDoubleTextAreaComponent).length).toEqual(1);
-    expect(wrapper.find('ObservationDoubleTextAreaComponent[leftValue="left value"]').length).toEqual(1);
-    expect(wrapper.find('ObservationDoubleTextAreaComponent[rightValue="right value"]').length).toEqual(1);
-    expect(wrapper.find('ObservationDoubleTextAreaComponent[leftPlaceHolder="Left placeholder"]').length).toEqual(1);
-    expect(wrapper.find('ObservationDoubleTextAreaComponent[rightPlaceHolder="Right placeholder"]').length).toEqual(1);
-    expect(wrapper.find('ObservationDoubleTextAreaComponent[leftTooltip="Left tooltip"]').length).toEqual(1);
-    expect(wrapper.find('ObservationDoubleTextAreaComponent[rightTooltip="Right tooltip"]').length).toEqual(1);
-    expect(wrapper.find(MusitDropDownField).length).toEqual(3);
-    expect(wrapper.find('MusitDropDownField[value="Adult"]').length).toEqual(2);
-    expect(wrapper.find('MusitDropDownField[value="Puppe"]').length).toEqual(1);
-    expect(wrapper.find(MusitField).length).toEqual(3);
-    expect(wrapper.find('MusitField[value="1"]').length).toEqual(1);
-    expect(wrapper.find('MusitField[value="10"]').length).toEqual(1);
-    expect(wrapper.find('MusitField[value="20"]').length).toEqual(1);
-    expect(wrapper.contains(<ControlLabel>LifeCycle label</ControlLabel>)).toEqual(true);
-    expect(wrapper.contains(<ControlLabel>Count label</ControlLabel>)).toEqual(true);
-    expect(wrapper.contains(<ControlLabel>LifeCycle label</ControlLabel>)).toEqual(true);
+    wrapper = render(
+        <RenderPest
+          index={1}
+          valueProps={ValueProps}
+          disabled={false}
+          layoutProps={layoutProps}
+          canEdit={false}
+        />
+      );
+  });
+
+  it('Create two textarea components', () => {
+    expect(wrapper.find('textarea').length).toBe(2);
+  });
+  it('Create input components', () => {
+    expect(wrapper.find('input').length).toBe(6);
+  });
+  it('Check Puppe', () => {
+    expect(wrapper.text().includes('Puppe')).toBe(true);
+  });
+  it('Check Larva', () => {
+    expect(wrapper.text().includes('Larve')).toBe(true);
+  });
+  it('Check Adult', () => {
+    expect(wrapper.text().includes('Adult')).toEqual(true);
+  });
+  it('Check identification label', () => {
+    expect(wrapper.text().includes('Identifikasjon')).toEqual(true);
+  });
+  it('Check comment label', () => {
+    expect(wrapper.text().includes('Tiltak/Kommentar')).toEqual(true);
+  });
+  it('Check count label', () => {
+    expect(wrapper.text().includes('Antall')).toEqual(true);
+  });
+  it('Create input value 1', () => {
+    expect(wrapper.find('input[value="1"]').length).toBe(1);
+  });
+  it('Create input value 20', () => {
+    expect(wrapper.find('input[value="20"]').length).toBe(1);
+  });
+  it('Create input value 10', () => {
+    expect(wrapper.find('input[value="10"]').length).toBe(1);
+  });
+  it('Check Identification value.', () => {
+    expect(wrapper.text().includes('Identification value.')).toEqual(true);
+  });
+  it('Check Comment value', () => {
+    expect(wrapper.text().includes('Comment value.')).toEqual(true);
   });
 });
