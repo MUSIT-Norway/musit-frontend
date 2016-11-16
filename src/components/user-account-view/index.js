a0147b5e1249897e7a9d7bf818dfacbd2bcf61b2
Stein Olsen <stein.olsen@usit.uio.no>
2016-11-16 09:25:21 +0100

    Setting placement on overlay trigger

--- src/components/user-account-view/index.js
+++ src/components/user-account-view/index.js
@@ -32,7 +32,7 @@ export default class MusitUserAccount extends Component {
     const currentLanguage = localStorage.getItem('language');
     const checked = (language) => currentLanguage === language && <FontAwesome name="check" /> ;
     const tooltip = 
-      <Tooltip id="tooltip" positionleft={500}>Logget inn som <strong>{this.props.user.userId}</strong></Tooltip>;
+      <Tooltip id="tooltip">Logget inn som <strong>{this.props.user.userId}</strong></Tooltip>;
     const menuText = (t1, t2) =>
       <Row>
         <Col md={1}>{t1}</Col>
