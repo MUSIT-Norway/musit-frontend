82334df53b788d9f240a9e635952060760f59723
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-11-23 11:44:42 +0100

    Fixing fake login

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   isDev: process.env.NODE_ENV === 'development',
-  isFake: process.env.REACT_APP_USE_DATAPORTEN !== 'true',
+  isFake: false,
   useDevTools: process.env.DEV_TOOLS || false,
   magasin: {
     urls: {
