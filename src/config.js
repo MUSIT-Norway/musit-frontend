84eda9ac6d66a9542e820b0e617bffcc86cf4a4f
Knut Petter Meen <kp@scalytica.net>
2016-11-23 09:47:23 +0100

    Correcting syntax for fake env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   isDev: process.env.NODE_ENV === 'development',
-  isFake: process.env.FAKE_FEIDE || true,
+  isFake: !process.env.USE_DATAPORTEN || true,
   useDevTools: process.env.DEV_TOOLS || false,
   magasin: {
     urls: {
