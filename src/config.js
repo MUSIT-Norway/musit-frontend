d4b9a668bd7f847cd2b134372c9e2d5c63bd4599
Stein Olsen <stein.olsen@usit.uio.no>
2016-11-24 09:40:44 +0100

    Lokal endring for meg

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   isDev: process.env.NODE_ENV === 'development',
-  isFake: process.env.FAKE_FEIDE || true,
+  isFake: process.env.FAKE_FEIDE || false,
   useDevTools: process.env.DEV_TOOLS || false,
   magasin: {
     urls: {
