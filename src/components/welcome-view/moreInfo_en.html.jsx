1a88d9bdbe8a131409b2c0f407c5b5ebf9b3f517
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-01-11 14:39:33 +0100

    lower to upper case

--- src/components/welcome-view/moreInfo_en.html.jsx
+++ src/components/welcome-view/moreInfo_en.html.jsx
@@ -4,12 +4,12 @@ import { Modal, Button } from 'react-bootstrap';
 export default (props) => (
   <Modal show={props.isVisible} onHide={props.hideModal}>
     <Modal.Header closeButton>
-      <Modal.Title>MUSITbasen og bruk av personopplysninger</Modal.Title>
+      <Modal.Title>MUSITBasen og bruk av personopplysninger</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <p>MUSITbasen benytter Dataporten til autentisering av brukere ved pålogging.</p>
+      <p>MUSITBasen benytter Dataporten til autentisering av brukere ved pålogging.</p>
       <br />
-      <p>I MUSITbasen lagrer vi følgende personopplysninger:</p>
+      <p>I MUSITBasen lagrer vi følgende personopplysninger:</p>
       <ul>
         <li>Brukernavn, fullt navn og epostadresse ved ditt universitet.</li>
         <li>Informasjon om hvilke tilganger en bruker har og hvilke endringer som er gjort i systemet av brukeren.</li>
