bb34ea56936245e7ae6c99cdc027aeb8944d347d
lasjo <l.a.sjo@usit.uio.no>
2017-01-12 12:36:41 +0100

    Update moreInfo_no.html.jsx

--- src/components/welcome-view/moreInfo_no.html.jsx
+++ src/components/welcome-view/moreInfo_no.html.jsx
@@ -4,12 +4,12 @@ import { Modal, Button } from 'react-bootstrap';
 export default (props) => (
   <Modal show={props.isVisible} onHide={props.hideModal}>
     <Modal.Header closeButton>
-      <Modal.Title>MUSITbasen og bruk av personopplysninger</Modal.Title>
+      <Modal.Title>Bruk av personopplysninger i MUSITbasen</Modal.Title>
     </Modal.Header>
     <Modal.Body>
       <p>MUSITbasen benytter Dataporten til autentisering av brukere ved pålogging.</p>
       <br />
-      <p>I MUSITbasen lagrer vi følgende personopplysninger:</p>
+      <p>I MUSITbasen lagres følgende personopplysninger:</p>
       <ul>
         <li>Brukernavn, fullt navn og epostadresse ved ditt universitet.</li>
         <li>Informasjon om hvilke tilganger en bruker har og hvilke endringer som er gjort i systemet av brukeren.</li>
