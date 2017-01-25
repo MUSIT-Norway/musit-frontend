da967b5ed793663fe5946c292d0b782b00472b83
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2017-01-25 17:45:44 +0100

    Found the implemented the logic to change text based upon URL. Continue working to change CSS.

--- src/modules/app/AppComponent.js
+++ src/modules/app/AppComponent.js
@@ -144,6 +144,10 @@ export class App extends Component {
         </div>
 
         <footer className="footer well version">
+          {window.location.href.includes('test:') ? 'DEV' : ''}
+          {window.location.href.includes('utv.uio.no') ? 'UTV' : ''}
+          {window.location.href.includes('test.uio.no') ? 'TEST' : ''}
+          {' '}
           {'Build number: ' + this.props.appSession.getBuildNumber()}
         </footer>
       </div>
