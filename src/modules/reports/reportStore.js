129db1cfb800c56b8898db5bbf9cc3853b1e8140
Stein Olsen <stein.olsen@usit.uio.no>
2017-01-31 15:52:13 +0100

    WIP

--- src/modules/reports/reportStore.js
+++ src/modules/reports/reportStore.js
@@ -1,5 +1,21 @@
 import { Observable, Subject } from 'rxjs';
 import { I18n } from 'react-i18nify';
 import { createStore } from 'react-rxjs/dist/RxStore';
+import { Reports } from '../../models/report';
 
-export const loadKDReport$  = new Subject().switchMap
+
+const initialState= { data: [] }
+
+export const clear$ = new Subject();
+
+export const loadKDReport$  = new Subject().switchMap(cmd =>
+  Reports(cmd.id).getKDReport(cmd.token).flatMap(rows => rows));
+
+
+export const reducer$ = (actions) =>
+  Observable.empty().merge(
+    actions.clear$.map(() => () => initialState),
+    actions.loadKDReport$.map((data) => (state) => ({...state, data, error: null}))
+  );
+
+export default createStore('Reports', reducer$({clear$, loadKDReport$}), Observable.of(initialState));
