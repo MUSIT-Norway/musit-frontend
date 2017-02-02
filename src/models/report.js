367a0ebe306090a12e6b6f7b8ece6a1ae0e44107
Stein Olsen <stein.olsen@usit.uio.no>
2017-02-02 08:49:29 +0100

    Linting

--- src/models/report.js
+++ src/models/report.js
@@ -2,17 +2,11 @@ import Config from '../config';
 import { apiUrl } from '../shared/util';
 
 class Reports {
-  constructor() {
-  }
 }
 
 Reports.getKDReport = (ajaxGet, token: string, id: number)  => {
-  console.log(`${token} ${id}   Hei`);
   const url = apiUrl(`${Config.magasin.urls.storagefacility.baseUrl(id)}/report`);
-  const ret = ajaxGet(url,token);
-
-  console.log('Retur', ret);
-  return ret;
+  return ajaxGet(url,token);
 };
 
 export default Reports;
