225ebb8e06752d7d10a62aa5080d4fe8a5378dee
Stein Olsen <stein.olsen@usit.uio.no>
2017-02-03 15:37:36 +0100

    WIP

---
+++ src/modules/reports/__tests__/reportStore-test.js
@@ -0,0 +1,52 @@
+import { TestScheduler, Subject } from 'rxjs/Rx';
+import assert from 'assert';
+import { reducer$ } from '../reportStore';
+import { createStore } from 'react-rxjs/dist/RxStore';
+
+describe('KDReportStore', () => {
+
+  it('testing reducer', () => {
+    const testScheduler = new TestScheduler(assert.deepEqual.bind(assert));
+
+    // mock streams
+    const nullMarbles       = '--------------';
+    const setLoadingM       = '-x------------';
+    const loadingChildrenM  = '------1--2----';
+    const expected          = 'ab----c--d----';
+
+    const expectedStateMap = {
+      a: {},
+      b: { data: {
+        kdreport:
+        {totalArea: '4666.3',
+          perimeterSecurity: '34.3',
+          theftProtection: '44.4',
+          fireProtection: '4566.333',
+          waterDamageAssessment:'344.3',
+          routinesAndContingencyPlan: '433.2'
+        }} },
+      c: { children: { loading: false, data: { matches: [], totalMatches: 0 }} },
+      d: { children: { loading: false, data: { matches: [{ name: 'en node', nodeId: 'uuid', id: 1}], totalMatches: 1 }} }
+    };
+
+    // mock up$ and down$ events
+    const clearRootNode$ = testScheduler.createHotObservable(nullMarbles);
+    const setLoading$ = testScheduler.createHotObservable(setLoadingM);
+    const loadNodes$ = testScheduler.createHotObservable(loadingChildrenM, {
+      1: { matches: [], totalMatches: 0},
+      2: { matches: [{ name: 'en node', nodeId: 'uuid', id: 1}], totalMatches: 1 }
+    });
+    const loadObjects$ = new Subject();
+    const loadStats$ = testScheduler.createHotObservable(nullMarbles);
+    const deleteNode$ = testScheduler.createHotObservable(nullMarbles);
+    const loadRootNode$ = testScheduler.createHotObservable(nullMarbles);
+
+    const state$ = reducer$({clearRootNode$, setLoading$, loadNodes$, loadObjects$, loadStats$, deleteNode$, loadRootNode$});
+
+    // assertion
+    testScheduler.expectObservable(createStore('test', state$)).toBe(expected, expectedStateMap);
+
+    // run tests
+    testScheduler.flush();
+  });
+});
