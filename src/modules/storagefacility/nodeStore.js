6a56906d37054d25e8c92eec5ed607db3c308692
Stein Olsen <stein.olsen@usit.uio.no>
2017-02-08 15:03:35 +0100

    WIP (nodeStore)

--- src/modules/storagefacility/nodeStore.js
+++ src/modules/storagefacility/nodeStore.js
@@ -1,17 +1,20 @@
 import MusitNode from '../../models/node';
 import { Observable } from 'rxjs';
 import { createStore, createAction } from 'react-rxjs/dist/RxStore';
-import { simpleGet, simplePost } from '../../shared/RxAjax';
+import * as ajax from '../../shared/RxAjax';
 import { mapToFrontend } from '../../models/mapper';
 
 export const clearNode$ = createAction('clearNode$');
 
+const loadNode =  ({ simpleGet }) => (cmd) =>  MusitNode.getNode(simpleGet) (cmd.nodeId, cmd.museumId, cmd.token, cmd);
+const addNode =  ({ simplePost }) => (cmd) =>  MusitNode.addNode(simplePost) (cmd.nodeId, cmd.museumId, cmd.token, cmd.data, cmd);
+
 export const loadNode$ = createAction('loadRootNode$').switchMap((cmd) =>
-  MusitNode.getNode(simpleGet)(cmd.nodeId, cmd.museumId, cmd.token, cmd)
+  loadNode(ajax) (cmd.nodeId, cmd.museumId, cmd.token, cmd)
 );
 
 export const addNode$ = createAction('addNode$').switchMap((cmd) =>
-  MusitNode.addNode(simplePost) (cmd.nodeId, cmd.museumId, cmd.token, cmd.data, cmd)
+  addNode(ajax) (cmd.nodeId, cmd.museumId, cmd.token, cmd.data, cmd)
 );
 
 export const updateState$ = createAction('updateState$').switchMap((cmd) =>
@@ -20,10 +23,12 @@ export const updateState$ = createAction('updateState$').switchMap((cmd) =>
 
 export const reducer$ = (actions) =>
   Observable.empty().merge(
-    actions.loadNode$.map((unit) => (state) => ({...state, unit})),
-    actions.addNode$.map((data) => (state) => ({ ...state, data, loading: false })),
-    actions.clearNode$.map((data) => (state) => ({ ...state, data, loading: false })),
-    actions.updateState$.map((data) => (state) => ({ ...state, data, loading: false }))
+    actions.loadNode$.map((data) => (state) => ({...state, unit: data})),
+    actions.addNode$.map((data) => (state) => ({ ...state, unit: data, loading: false })),
+    actions.clearNode$.map(() => () => ({ unit: { environmentRequirement: {},
+                                                          environmentAssessment: {},
+                                                          securityAssessment: {} },  loading: false })),
+    actions.updateState$.map((data) => (state) => ({ ...state, unit: data, loading: false }))
   );
 
 export default createStore('node', reducer$({clearNode$,  updateState$, loadNode$,  addNode$ }), Observable.of({
