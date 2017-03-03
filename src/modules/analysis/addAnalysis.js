a910fb2f0933642e57c49c97449999ffc6f5da93
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2017-03-03 11:16:02 +0100

    WIP

--- src/modules/analysis/addAnalysis.js
+++ src/modules/analysis/addAnalysis.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import {PageHeader, Form, FormGroup, Col, DropdownButton, FormControl, MenuItem, ControlLabel, InputGroup} from 'react-bootstrap';
+import {
+  PageHeader,
+  Form,
+  FormGroup,
+  Col,
+  DropdownButton,
+  FormControl,
+  MenuItem,
+  ControlLabel,
+  InputGroup
+} from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 
 function FieldGroup({id, label, ...props}) {
-  return(
+  return (
     <FormGroup controlId={id}>
       <Col componentClass={ControlLabel} md={1}>{label}</Col>
-      <Col  md={2}>
+      <Col md={2}>
         <FormControl {... props} />
       </Col>
     </FormGroup>
@@ -18,63 +28,73 @@ const AddAnalysis = () => {
     <Form>
       <br/>
       <PageHeader>{'Registrere analyse'}</PageHeader>
-       <FormGroup>
-          <Col md={12}><b> HID:: </b>123</Col>
-        </FormGroup>
-        <FormGroup>
-          <Col md={2}>
-            <b>Registrert:</b>
-          </Col>
-          <Col md={2}>
-            <FontAwesome name='user'/>{' '}
-            {'Per Hansen'}
-          </Col>
-          <Col md={8}>
-            <FontAwesome name='clock-o'/>{' '}
-            {'15.12.2017'}
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Col md={2}>
-            <b>Sist endret:</b>
-          </Col>
-          <Col md={2}>
-            <FontAwesome name='user'/>{' '}
-            {'Per Hansen'}
-          </Col>
-          <Col md={2}>
-            <FontAwesome name='clock-o'/>{' '}
-            {'15.12.2017'}
-          </Col>
-          <Col md={6}>
-            <a href=''>Se endringshistorikk</a>
-          </Col>
-        </FormGroup>
+      <FormGroup>
+        <Col md={12}><b> HID:: </b>123</Col>
+      </FormGroup>
+      <FormGroup>
+        <Col md={2}>
+          <b>Registrert:</b>
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col md={2}>
+          <FontAwesome name='user'/>{' '}
+          Per Hansen
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col md={8}>
+          <FontAwesome name='clock-o'/>{' '}
+          15.12.2017
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col md={2}>
+          <b>Sist endret:</b>
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col md={2}>
+          <FontAwesome name='user'/>{' '}
+          Per Hansen
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col md={2}>
+          <FontAwesome name='clock-o'/>{' '}
+          15.12.2017
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col md={6}>
+          <a href=''>Se endringshistorikk</a>
+        </Col>
+      </FormGroup>
       <hr/>
       <br/>
-        <FieldGroup
-          id="formControlsText"
-          type="text"
-          label="Text"
-          placeholder="Enter text"
-        />
-         <hr/>
-        <InputGroup style={{minWidth:'796px'}}>
-          <Col md={4}>
-            <b>Analysetype</b>{' '}
-            <DropdownButton bsStyle={'Default'} title='Velg type' id="bg-nested-dropdown">
-              <MenuItem eventKey="1">Vev</MenuItem>
-              <MenuItem eventKey="2">Blad</MenuItem>
-            </DropdownButton>
-          </Col>
-          <Col md={4}>
-            <b>Analyseundertype</b>{' '}
-            <DropdownButton bsStyle={'Default'} title='Velg type' id="bg-nested-dropdown">
-              <MenuItem eventKey="1">Tallus</MenuItem>
-              <MenuItem eventKey="2">Klorofyll</MenuItem>
-            </DropdownButton>
-          </Col>
-        </InputGroup>
+      <FieldGroup
+        id="formControlsText"
+        type="text"
+        label="Text"
+        placeholder="Enter text"
+      />
+      <hr/>
+      <InputGroup style={{minWidth:'796px'}}>
+        <Col md={4}>
+          <b>Analysetype</b>{' '}
+          <DropdownButton bsStyle={'Default'} title='Velg type' id="bg-nested-dropdown">
+            <MenuItem eventKey="1">Vev</MenuItem>
+            <MenuItem eventKey="2">Blad</MenuItem>
+          </DropdownButton>
+        </Col>
+        <Col md={4}>
+          <b>Analyseundertype</b>{' '}
+          <DropdownButton bsStyle={'Default'} title='Velg type' id="bg-nested-dropdown">
+            <MenuItem eventKey="1">Tallus</MenuItem>
+            <MenuItem eventKey="2">Klorofyll</MenuItem>
+          </DropdownButton>
+        </Col>
+      </InputGroup>
       <br/>
       <Form inline>
         <FormGroup style={{minWidth:'796px'}}>
@@ -96,7 +116,7 @@ const AddAnalysis = () => {
             <ControlLabel>Analysevolum/-vekt</ControlLabel>
           </Col>
           <Col md={3}>
-            <FormControl type="text" placeholder="Vekt/Volum" />
+            <FormControl type="text" placeholder="Vekt/Volum"/>
           </Col>
           <Col md={3}>
             <DropdownButton bsStyle={'Default'} title='Velg måleenhet' id="bg-nested-dropdown">
