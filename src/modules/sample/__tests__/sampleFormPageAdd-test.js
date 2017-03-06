afc5822e44a5b23eb242cf14e7260033c7dc0f58
Stein Olsen <stein.olsen@usit.uio.no>
2017-03-06 15:39:51 +0100

    Adding test

---
+++ src/modules/sample/__tests__/sampleFormPageAdd-test.js
@@ -0,0 +1,16 @@
+/**
+ * Created by steinaol on 06.03.17.
+ */
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+import React from 'react';
+import SampleFormPageAdd from '../SampleFormPageAdd';
+
+
+describe('AnalysisSampleFormPageAdd', () => {
+
+  it('should display correctly', () => {
+    const wrapper=shallow(<SampleFormPageAdd/>);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
