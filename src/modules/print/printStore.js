c23e465d308dc5f8d9435353adf4c2d679d94540
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-03-07 20:47:20 +0100

    Moving template api calls to model

--- src/modules/print/printStore.js
+++ src/modules/print/printStore.js
@@ -1,24 +1,11 @@
 import { Observable } from 'rxjs';
 import { createAction, createStore } from 'react-rxjs/dist/RxStore';
-import * as ajaxMethods from '../../shared/RxAjax';
-import Config from '../../config';
 import Template from '../../models/template';
-
-export const loadTemplates = ({ simpleGet }) => ({ token }) =>
-  simpleGet(Config.magasin.urls.api.barcode.templatesUrl, token)
-    .map(({ response }) => response && response.map(json => new Template(json)));
-
-export const renderTemplate = ({ ajax }) => ({ templateId, codeFormat, nodes, token }) => {
-  const data = nodes.map(node => ({ uuid: node.uuid, data: [{ field: 'name', value: node.name }] }));
-  const url = Config.magasin.urls.api.barcode.templateRenderUrl(templateId, codeFormat);
-  return ajax(url, 'POST', data, token, { 'Content-Type': 'application/json' }, 'text/html')
-    .map(({response}) => response);
-};
 
 export const clearAll$ = createAction('clearAll$');
 export const clearRendered$ = createAction('clearRendered$');
-export const loadTemplates$ = createAction('loadTemplates$').switchMap(loadTemplates(ajaxMethods));
-export const renderTemplate$ = createAction('renderTemplate$').switchMap(renderTemplate(ajaxMethods));
+export const loadTemplates$ = createAction('loadTemplates$').switchMap(Template.loadTemplates());
+export const renderTemplate$ = createAction('renderTemplate$').switchMap(Template.renderTemplate());
 
 const initialState = { templates: [], rendered: null };
 
