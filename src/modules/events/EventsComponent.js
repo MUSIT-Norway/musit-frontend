f424ac0fae7fc3f094bece2ea991cc31fa22aaf6
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-03-22 14:28:50 +0100

    Table and server call implemented

--- src/modules/events/EventsComponent.js
+++ src/modules/events/EventsComponent.js
@@ -1,32 +1,36 @@
-// @flow
-import React  from 'react';
-import { Table } from 'react-bootstrap';
+import React from 'react';
+const Table = require('reactable').Table;
+const Tr = require('reactable').Tr;
+const Td = require('reactable').Td;
+import './eventsComponent.css';
 
-type Props = {
-  events: any[]
-}
-
-export const EventsComponent = (props: Props) => (
-  <Table striped condensed>
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Type</th>
-        <th>Date</th>
-        <th>By</th>
-      </tr>
-    </thead>
-    <tbody>
-    {props.events.map((event, i) =>
-      <tr key={i}>
-        <td>{event.id}</td>
-        <td>{event.type}</td>
-        <td>{event.registeredDate}</td>
-        <td>{event.registeredBy}</td>
-      </tr>
-    )}
-    </tbody>
-  </Table>
+export const EventsComponent = (props) => (
+  <div>
+    <br />
+    <Table
+      className="table"
+      columns={[
+        { key: 'eventDate', label: 'Dato'},
+        { key: 'type', label: 'Type hendelse'},
+        { key: 'registeredBy', label: 'Utført av' },
+        { key: 'note', label: 'Kommentar' },
+        { key: 'id', label: '' }
+      ]}
+      sortable={['type', 'eventDate', 'registeredBy', 'note' ]}
+      defaultSort={{ column: 'eventDate', direction: 'desc' }}
+      noDataText="Ingen hendelser funnet på objektet"
+    >
+      {props.events && props.events.map((event, i) =>
+        <Tr key={i}>
+          <Td column="eventDate">{event.eventDate}</Td>
+          <Td column="type">{event.type}</Td>
+          <Td column="registeredBy">{event.registeredBy}</Td>
+          <Td column="note">{event.note || ''}</Td>
+          <Td column="id"><a href="#" onClick={(e) => e.preventDefault()}>Les mer</a></Td>
+        </Tr>
+      )}
+    </Table>
+  </div>
 );
 
 export default EventsComponent;
--- src/modules/events/eventsStore.js
+++ src/modules/events/eventsStore.js
@@ -3,16 +3,42 @@ import { createStore, createAction } from 'react-rxjs/dist/RxStore';
 import { Observable } from 'rxjs';
 import Analyses from '../../models/analyses';
 import MusitObject from '../../models/object';
+import { parseISODate, DATE_FORMAT_DISPLAY } from '../../shared/util';
+import MusitActor from '../../models/actor';
+import uniq from 'lodash/uniq';
+import { I18n } from 'react-i18nify';
 
 const initialState = [];
+
+const formatEventDate = (event) => parseISODate(event.registeredDate).format(DATE_FORMAT_DISPLAY);
 
 const concatAnalysesWithMoves = (val) =>
   Observable.forkJoin(
     Analyses.getAnalysesForObject()(val),
-    MusitObject.getLocationHistory()(val)
-  ).map(([analyses, moves]) =>
-    analyses.concat(moves.map(m => ({...m, type: 'Move'})))
-  );
+    MusitObject.getLocationHistory()(val).map(locations =>
+      locations.map(loc => ({...loc, type: 'MoveObject' }))
+    )
+  ).map(([analyses, moves]) => {
+    const events = analyses.concat(moves);
+    return events.map(m => ({...m, eventDate: formatEventDate(m)}));
+  })
+  .flatMap((events) => {
+    const actorIds = uniq(events.map(r => r.registeredBy )).filter(r => r);
+    const eventsWithNames = MusitActor.getActors()(actorIds, val.token)
+     .map(actors => {
+       if (!Array.isArray(actors)) {
+         return events;
+       }
+       return events.map((data) => {
+         const registeredBy = actors.find(a => a.hasActorId(data.registeredBy));
+         return {
+           ...data,
+           registeredBy: registeredBy ? registeredBy.fn : I18n.t('musit.unknown')
+         };
+       });
+     });
+    return eventsWithNames.map(arr => arr.map(m => ({...m, eventDate: formatEventDate(m)})));
+  });
 
 export const loadAnalyses$ = createAction('loadAnalyses$').switchMap(concatAnalysesWithMoves);
 
