cfcd7be4949151ca2dbc62e212b37df4d57be623
Ingar Abrahamsen <ingar@abrahams1.com>
2017-06-23 13:20:13 +0200

    feat: throw an error if lifecycle is used wrong
    I thought that flow would have cached this kind of errors but it doesn't.
    Since all the properties are optional it's perfectly ok to send in an
    function since it doesn't break the contract. Since this have been changed
    from taking an function to an object we' will not detect the faulty
    usage when used wrong. This will at least give us a hard fault when
    creating the component.

--- src/shared/lifeCycle.js
+++ src/shared/lifeCycle.js
@@ -1,17 +1,24 @@
 // @flow
 import React from 'react';
 
-type Methods = {
+export type Methods = {
   onMount?: (props: *) => void,
   onReceiveProps?: (props: *) => void,
   onUnmount?: (props: *) => void
 };
 
-export default ({
-  onMount = () => {},
-  onReceiveProps = () => {},
-  onUnmount = () => {}
-}: Methods) => (Component: any) => {
+const noop = props => {};
+
+export default (methods: Methods) => (Component: any) => {
+  if (typeof methods === 'function') {
+    throw new Error(
+      'Illegal lifecycle argument, expected an object but found a function.'
+    );
+  }
+  const onMount = methods.onMount || noop;
+  const onUnmount = methods.onUnmount || noop;
+  const onReceiveProps = methods.onReceiveProps || noop;
+
   class MountWrapper extends React.Component {
     props: *;
 
