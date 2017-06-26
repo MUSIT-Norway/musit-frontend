2376e9e05145e6f500bd44156a0458c6824ae291
Ingar Abrahamsen <ingar@abrahams1.com>
2017-06-26 12:28:33 +0200

    fix: clearing expiration date
    The DatePicker component wasn't wired up correct. It did miss the onClear
    props. That also introduced an issue where we want to blank out the value.
    
    The DatePicker component is behaving a bit strange here and it looks like
    it's not happy that we're clearing the value. Since we're wrapping the
    component in the first place we probably should move the logic from the
    restriction component into the date picker component.

--- src/modules/analysis/components/Restrictions.js
+++ src/modules/analysis/components/Restrictions.js
@@ -93,11 +93,19 @@ export default function Restrictions({ appSession, form, updateForm }: Props) {
           <DatePicker
             dateFormat={DATE_FORMAT_DISPLAY}
             value={form.restrictions_expirationDate.rawValue || ''}
-            onChange={e =>
+            onClear={() =>
               updateForm({
                 name: form.restrictions_expirationDate.name,
-                rawValue: formatISOString(e)
+                rawValue: null
               })}
+            onChange={selectedDate => {
+              if (selectedDate !== 'Invalid date') {
+                updateForm({
+                  name: form.restrictions_expirationDate.name,
+                  rawValue: formatISOString(selectedDate)
+                });
+              }
+            }}
           />
         </div>
       </div>
--- src/modules/analysis/components/__tests__/Restriction-test.js
+++ src/modules/analysis/components/__tests__/Restriction-test.js
@@ -3,6 +3,7 @@
 import React from 'react';
 
 import { getArrField, getStrField } from '../../../../forms/form';
+import DatePicker from '../../../../components/DatePicker';
 import Restriction from '../Restrictions';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
@@ -36,6 +37,52 @@ describe('Restriction component', () => {
     expect(updateForm.getCall(0).args[0]).toEqual({
       name: form.restrictions_reason.name,
       rawValue: expectedValue
+    });
+  });
+
+  describe('expiration date field', () => {
+    it('should sett form value to null when clearing date', () => {
+      const appSession = sinon.spy();
+      const updateForm = sinon.spy();
+      const Comp = shallow(
+        <Restriction form={form} updateForm={updateForm} appSession={appSession} />
+      );
+
+      Comp.find(DatePicker).props().onClear();
+
+      expect(updateForm.calledOnce).toBe(true);
+      expect(updateForm.getCall(0).args[0]).toEqual({
+        name: form.restrictions_expirationDate.name,
+        rawValue: null
+      });
+    });
+
+    it('should update form value with the date when its valid', () => {
+      const appSession = sinon.spy();
+      const updateForm = sinon.spy();
+      const Comp = shallow(
+        <Restriction form={form} updateForm={updateForm} appSession={appSession} />
+      );
+
+      Comp.find(DatePicker).props().onChange('2017-06-14T00:00:00.000+02:00');
+
+      expect(updateForm.calledOnce).toBe(true);
+      expect(updateForm.getCall(0).args[0]).toEqual({
+        name: form.restrictions_expirationDate.name,
+        rawValue: '2017-06-14T00:00:00.000+02:00'
+      });
+    });
+
+    it('should not trigger update form value when the date is invalid', () => {
+      const appSession = sinon.spy();
+      const updateForm = sinon.spy();
+      const Comp = shallow(
+        <Restriction form={form} updateForm={updateForm} appSession={appSession} />
+      );
+
+      Comp.find(DatePicker).props().onChange('Invalid date');
+
+      expect(updateForm.notCalled).toBe(true);
     });
   });
 });
