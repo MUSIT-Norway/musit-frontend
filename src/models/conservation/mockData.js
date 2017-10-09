5bec0f53fc7cb703417a70c086343a89a72ed4e1
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2017-10-09 13:32:37 +0200

    Added the JSON with sub events in mock data

--- src/models/conservation/mockData.js
+++ src/models/conservation/mockData.js
@@ -93,3 +93,249 @@ export const conservationEventTypeMockData = {
     }
   ]
 };
+
+export const getJson = {
+  id: 166,
+  eventTypeId: 1, // conservation event
+
+  // below two are auto created when when we do the post
+  registeredBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+  registeredDate: '2017-09-20T13:39:51+00:00',
+
+  // persons can be different based upon requirements
+  doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+  doneDate: '2017-09-11T22:00:00+00:00',
+  completedBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+  completedDate: '2017-09-19T22:00:00+00:00',
+
+  // attributes of main event
+  note: 'Test note',
+  caseNumber: 'test case number. 123',
+  affectedThings: [
+    '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+    'aba6a67c-f742-4a44-b13e-0415ec1abb2a',
+    '788136fe-8038-47fc-9c59-9ddb010e0f74',
+    'fa32417b-89ec-4d27-8f4b-27f3a2b03583'
+  ],
+
+  // below two are auto created when we do the put(will not come after post)
+  updatedBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+  updatedDate: '2017-10-09T08:17:02+00:00',
+
+  // sub events
+  events: [
+    {
+      id: 167, // sub event id
+      eventTypeId: 10, // technical description
+      partOf: 166, // pointer to main event
+
+      registeredBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      registeredDate: '2017-09-20T13:39:51+00:00',
+      // below two are auto created when we do the put(will not come after post)
+      updatedBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      updatedDate: '2017-10-09T08:17:02+00:00',
+
+      // can have sub set of main event affectedThings
+      affectedThings: ['12080e3e-2ca2-41b1-9d4a-4d72e292dcd8'],
+      // sub event specific attributes
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      id: 168,
+      eventTypeId: 10, // technical description number 2
+      partOf: 166,
+
+      registeredBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      registeredDate: '2017-09-20T13:39:51+00:00',
+      // below two are auto created when we do the put(will not come after post)
+      updatedBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      updatedDate: '2017-10-09T08:17:02+00:00',
+
+      affectedThings: [
+        '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+        'aba6a67c-f742-4a44-b13e-0415ec1abb2a'
+      ],
+
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      id: 169,
+      eventTypeId: 12,
+      partOf: 166,
+      registeredBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      registeredDate: '2017-09-20T13:39:51+00:00',
+      // below two are auto created when we do the put(will not come after post)
+      updatedBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      updatedDate: '2017-10-09T08:17:02+00:00',
+
+      affectedThings: ['788136fe-8038-47fc-9c59-9ddb010e0f74'],
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      id: 170,
+      eventTypeId: 13,
+      partOf: 166,
+      registeredBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      registeredDate: '2017-09-20T13:39:51+00:00',
+      // below two are auto created when we do the put(will not come after post)
+      updatedBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      updatedDate: '2017-10-09T08:17:02+00:00',
+
+      affectedThings: [
+        '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+        'aba6a67c-f742-4a44-b13e-0415ec1abb2a',
+        '788136fe-8038-47fc-9c59-9ddb010e0f74',
+        'fa32417b-89ec-4d27-8f4b-27f3a2b03583'
+      ],
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    }
+  ]
+};
+
+export const postJson = {
+  eventTypeId: 1, // conservation event
+
+  // persons can be different based upon requirements
+  doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+  doneDate: '2017-09-11T22:00:00+00:00',
+  completedBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+  completedDate: '2017-09-19T22:00:00+00:00',
+
+  // attributes of main event
+  note: 'Test note',
+  caseNumber: 'test case number. 123',
+  affectedThings: [
+    '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+    'aba6a67c-f742-4a44-b13e-0415ec1abb2a',
+    '788136fe-8038-47fc-9c59-9ddb010e0f74',
+    'fa32417b-89ec-4d27-8f4b-27f3a2b03583'
+  ],
+
+  // sub events
+  events: [
+    {
+      eventTypeId: 10, // technical description
+
+      // can have sub set of main event affectedThings
+      affectedThings: ['12080e3e-2ca2-41b1-9d4a-4d72e292dcd8'],
+      // sub event specific attributes
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      eventTypeId: 10, // technical description number 2
+      affectedThings: [
+        '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+        'aba6a67c-f742-4a44-b13e-0415ec1abb2a'
+      ],
+
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      eventTypeId: 12,
+      affectedThings: ['788136fe-8038-47fc-9c59-9ddb010e0f74'],
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      eventTypeId: 13,
+      affectedThings: [
+        '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+        'aba6a67c-f742-4a44-b13e-0415ec1abb2a',
+        '788136fe-8038-47fc-9c59-9ddb010e0f74',
+        'fa32417b-89ec-4d27-8f4b-27f3a2b03583'
+      ],
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    }
+  ]
+};
+
+export const putJson = {
+  id: 166,
+  eventTypeId: 1, // conservation event
+
+  // persons can be different based upon requirements
+  doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+  doneDate: '2017-09-11T22:00:00+00:00',
+  completedBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+  completedDate: '2017-09-19T22:00:00+00:00',
+
+  // attributes of main event
+  note: 'Test note',
+  caseNumber: 'test case number. 123',
+  affectedThings: [
+    '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+    'aba6a67c-f742-4a44-b13e-0415ec1abb2a',
+    '788136fe-8038-47fc-9c59-9ddb010e0f74',
+    'fa32417b-89ec-4d27-8f4b-27f3a2b03583'
+  ],
+
+  // sub events
+  events: [
+    {
+      id: 167, // sub event id
+      eventTypeId: 10, // technical description
+      partOf: 166, // pointer to main event
+
+      // can have sub set of main event affectedThings
+      affectedThings: ['12080e3e-2ca2-41b1-9d4a-4d72e292dcd8'],
+      // sub event specific attributes
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      id: 168,
+      eventTypeId: 10, // technical description number 2
+      partOf: 166,
+
+      affectedThings: [
+        '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+        'aba6a67c-f742-4a44-b13e-0415ec1abb2a'
+      ],
+
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      id: 169,
+      eventTypeId: 12,
+      partOf: 166,
+
+      affectedThings: ['788136fe-8038-47fc-9c59-9ddb010e0f74'],
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    },
+    {
+      id: 170,
+      eventTypeId: 13,
+      partOf: 166,
+
+      affectedThings: [
+        '12080e3e-2ca2-41b1-9d4a-4d72e292dcd8',
+        'aba6a67c-f742-4a44-b13e-0415ec1abb2a',
+        '788136fe-8038-47fc-9c59-9ddb010e0f74',
+        'fa32417b-89ec-4d27-8f4b-27f3a2b03583'
+      ],
+      note: 'fdfds',
+      doneBy: '7dcc7e82-a18c-4e2e-9d83-2b25c132fc3e',
+      doneDate: '2017-09-11T22:00:00+00:00'
+    }
+  ]
+};
