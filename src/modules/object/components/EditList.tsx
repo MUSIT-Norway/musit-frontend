39646f0c833384fb6d9dade217b5f8560c456f35
Anuradha Warusawithana <anuradha.warusawithana@usit.uio.no>
2018-08-09 13:57:41 +0200

    added new Database list | url

--- src/modules/object/components/EditList.tsx
+++ src/modules/object/components/EditList.tsx
@@ -1,38 +1,39 @@
 import * as React from 'react';
 import { AsyncCreatable } from 'react-select';
-import { InputGroup } from 'react-bootstrap';
 
 export type databaseOption = {
   value: string;
   label: string;
+  url: string;
 };
 
 export type databaseOptions = databaseOption[];
 
-const getOptions = (data: databaseOptions) => (input: string, callback: Function) => {
-  setTimeout(() => {
-    callback(null, {
-      options: data,
-      // CAREFUL! Only set this to true when there are no more options,
-      // or more specific queries will not be sent to the server.
-      complete: true
-    });
-  }, 500);
-};
+  const getOptions = (data: databaseOptions) => (input: string, callback: Function)  =>  {
+    setTimeout(() => {
+      callback(null, {
+        options:data,        
+        // CAREFUL! Only set this to true when there are no more options,
+        // or more specific queries will not be sent to the server.
+        complete: false
+      });
+    }, 500);
+  };
 
-export type editListProps = {
-  dataBaseValues: databaseOptions;
-  editingValue: string;
-  onChangeSelection: (inputValue: databaseOption) => void;
-};
+ export type editListProps = {
+    dataBaseValues: databaseOptions;
+    editingValue: string;
+    onChangeSelection: (inputValue: databaseOption) => void;
+  }
 
-export const EditList = (props: editListProps) => (
-  <div>
+export const EditList = ( props: editListProps) => (
+ <div>
     <AsyncCreatable
       name="editListDatabase"
       loadOptions={getOptions(props.dataBaseValues)}
       value={props.editingValue}
       onChange={props.onChangeSelection}
+      clearable={false}
     />
-  </div>
+ </ div>
 );
