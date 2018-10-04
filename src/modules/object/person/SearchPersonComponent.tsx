cb265e775e6af54ab864265f3fd8edfc9f27d315
Stein Olsen <stein.olsen@usit.uio.no>
2018-10-04 16:40:07 +0200

    Have a working searchpage

--- src/modules/object/person/SearchPersonComponent.tsx
+++ src/modules/object/person/SearchPersonComponent.tsx
@@ -3,6 +3,8 @@ import { OutputPerson } from '../../../models/object/person';
 import { GetPersonsFromPersonNameProps, PersonStoreState } from './PersonStore';
 import { AppSession } from '../../../types/appSession';
 import { AjaxGet } from '../../../types/ajax';
+import config from '../../../config';
+import { History } from 'history';
 
 export type SearchState = {
   searchString: string;
@@ -14,6 +16,7 @@ export type SearchProps = {
   ) => (s: GetPersonsFromPersonNameProps) => OutputPerson[];
   store: PersonStoreState;
   appSession: AppSession;
+  history: History;
 };
 
 export default class SearchPersonComponent extends React.Component<
@@ -30,47 +33,78 @@ export default class SearchPersonComponent extends React.Component<
   render() {
     return (
       <div>
-        <div className="input-group">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Search"
-            id="searcField"
-          />
-          <div className="input-group-btn">
-            <button
-              className="btn btn-default"
-              onClick={() => {
-                console.log('Klikkkkkk');
-                this.props.getPersonsFromPersonName()({
-                  name: 'Kars',
-                  collectionId: this.props.appSession.collectionId,
-                  token: this.props.appSession.accessToken
-                });
-              }}
-            >
-              Søk
-            </button>
+        <form>
+          <div className="container">
+            <div className="form-group">
+              <input
+                className="form-control"
+                id="searcField"
+                onChange={e => {
+                  const value = e.target.value;
+                  console.log('E', e.target.value);
+
+                  this.setState((ps: SearchState) => ({
+                    ...ps,
+                    searchString: value
+                  }));
+                }}
+              />
+              <div className="input-group-btn">
+                <button
+                  className="btn btn-default"
+                  type="button"
+                  onClick={() => {
+                    this.props.getPersonsFromPersonName()({
+                      name: this.state.searchString,
+                      collectionId: this.props.appSession.collectionId,
+                      token: this.props.appSession.accessToken
+                    });
+                  }}
+                >
+                  Søk
+                </button>
+              </div>
+            </div>
+            <table className="table">
+              <thead>
+                <th>Name</th>
+                <th>First name</th>
+                <th>Last name</th>
+              </thead>
+              <tbody>
+                {this.props.store.personList ? (
+                  this.props.store.personList.map((p: OutputPerson, i: number) => (
+                    <tr key={`SR-${i}`}>
+                      <td>{p.name}</td>
+                      <td>{p.firstName}</td>
+                      <td>{p.lastName}</td>
+                      <td>
+                        <button
+                          className="btn btn-link"
+                          type="button"
+                          onClick={e => {
+                            const url = config.magasin.urls.client.person.viewPerson(
+                              this.props.appSession,
+                              p.personUuid ? p.personUuid : '' //  r.response.personUuid  //
+                            );
+                            e.preventDefault();
+                            this.props.history && this.props.history.push(url);
+                          }}
+                        >
+                          Vis
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td>No data</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
           </div>
-        </div>
-        <table>
-          <thead>
-            <th>Name</th>
-          </thead>
-          <tbody>
-            {this.props.store.personList ? (
-              this.props.store.personList.map((p: OutputPerson) => (
-                <tr>
-                  <td>{p.name}</td>{' '}
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td>No data</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+        </form>
       </div>
     );
   }
